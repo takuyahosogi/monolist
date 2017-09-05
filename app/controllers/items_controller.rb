bcf0f09f9c8be19ac4857a24c9ecd61bfb412a04
takuya hosogi <t.hosogi26@gmail.com>
2017-09-05 16:00:10 +0000

    rakuten api

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,23 @@
+class ItemsController < ApplicationController
+  before_action :require_user_logged_in
+  def new
+    @items = []
+
+    @keyword = params[:keyword]
+    if @keyword
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits: 20,
+      })
+
+      results.each do |result|
+        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
+        item = Item.new(read(result))
+        @items << item
+      end
+    end
+  end
+  
+  
+end
