0b0e79af3780e4a55c2c4c4634524a95d31877b0
takuya hosogi <t.hosogi26@gmail.com>
2017-09-06 02:44:23 +0000

    have

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :require_user_logged_in
+
   def new
     @items = []
 
@@ -12,12 +13,17 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
   end
   
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+    @have_users = @item.have.users
+  end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show]
   def show
     @user = User.find(params[:id])
+    @items = @user.items.uniq
+    @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
   end
 
   def new
