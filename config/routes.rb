d5b42cd8eba2bf2fb310ffb4b2ee0f165e0c01e9
takuya hosogi <t.hosogi26@gmail.com>
2017-09-09 16:38:42 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create]
   
   get 'rankings/want', to: 'rankings#want'
+  
   get 'rankings/have', to: 'rankings#have'
   
   resources :items, only: [:show, :new]
